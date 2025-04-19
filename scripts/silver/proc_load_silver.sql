7c7755eb3cfec68d4992792039da84259457d2b0
Eike Hirsch <108685558+eikehirsch@users.noreply.github.com>
2025-04-19 18:11:52 -0300

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,19 +1,20 @@
 /*
 ===============================================================================
-Stored Procedure: Load Silver Layer (Source -> Silver)
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'silver' schema from external CSV files. 
-    It performs the following actions:
-    - Truncates the silver tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to silver tables.
-
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
 Parameters:
     None. 
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC silver.load_silver;
+    EXEC Silver.load_silver;
 ===============================================================================
 */
 CREATE OR ALTER PROCEDURE silver.load_silver AS
