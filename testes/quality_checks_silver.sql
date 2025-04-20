046cf096a07f1eb7dcd50b39485f00e254959026
Eike Hirsch <108685558+eikehirsch@users.noreply.github.com>
2025-04-20 14:59:49 -0300

    Update quality_checks_silver.sql

--- testes/quality_checks_silver.sql
+++ testes/quality_checks_silver.sql
@@ -1,27 +1,27 @@
 /*
 ===============================================================================
-Quality Checks
+Testes de qualidade
 ===============================================================================
-Script Purpose:
-    This script performs various quality checks for data consistency, accuracy, 
-    and standardization across the 'silver' layer. It includes checks for:
-    - Null or duplicate primary keys.
-    - Unwanted spaces in string fields.
-    - Data standardization and consistency.
-    - Invalid date ranges and orders.
-    - Data consistency between related fields.
+Objetivo do Script:
+    Esse script executa vários testes de qualidade de consistência e padronização  
+    dos dados na camada 'silver'. Isso inclui checagens para:
+    - Chaves-primárias nulas ou duplicadas.
+    - Espaços desnecessários em campos de strings.
+    - Consistência e padronização dos dados.
+    - Pedidos e abrangêngia de datas inválidos.
+    - Inconsistência de dados entre campos relacionados.
 
-Usage Notes:
-    - Run these checks after data loading Silver Layer.
-    - Investigate and resolve any discrepancies found during the checks.
+Notas para uso:
+    - Rodar essas checagens depois do carregamento da Camada Silver.
+    - Investigar e resolver quaisquer discrepâncias encontradas durantes as checagens.
 ===============================================================================
 */
 
 -- ====================================================================
--- Checking 'silver.crm_cust_info'
+-- Checando 'silver.crm_cust_info'
 -- ====================================================================
--- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
+-- Checagem de Chaves-primárias NULAS ou Duplicadas
+-- Esperado: 0 resultados
 SELECT 
     cst_id,
     COUNT(*) 
@@ -29,23 +29,23 @@ FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
--- Check for Unwanted Spaces
--- Expectation: No Results
+-- Checagem para espaços desnecessários
+-- Esperado: 0 resultados
 SELECT 
     cst_key 
 FROM silver.crm_cust_info
 WHERE cst_key != TRIM(cst_key);
 
--- Data Standardization & Consistency
+-- Consistência de padronização dos dados
 SELECT DISTINCT 
     cst_marital_status 
 FROM silver.crm_cust_info;
 
 -- ====================================================================
--- Checking 'silver.crm_prd_info'
+-- Checando 'silver.crm_prd_info'
 -- ====================================================================
--- Check for NULLs or Duplicates in Primary Key
--- Expectation: No Results
+-- Checagem para Chaves-primárias NULAS ou Duplicadas
+-- Esperado: 0 resultados
 SELECT 
     prd_id,
     COUNT(*) 
@@ -53,26 +53,26 @@ FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
--- Check for Unwanted Spaces
--- Expectation: No Results
+-- Checagem para espaços desnecessários
+-- Esperado: 0 resultados
 SELECT 
     prd_nm 
 FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm);
 
--- Check for NULLs or Negative Values in Cost
--- Expectation: No Results
+-- Checagem para valores NULOS ou Negativos na coluna Cost
+-- Esperado: 0 resultados
 SELECT 
     prd_cost 
 FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
--- Data Standardization & Consistency
+-- Consistência e padronização dos dados
 SELECT DISTINCT 
     prd_line 
 FROM silver.crm_prd_info;
 
--- Check for Invalid Date Orders (Start Date > End Date)
+-- Checagem de datas históricas dos produtos (Start Date > End Date)
 -- Expectation: No Results
 SELECT 
     * 
@@ -80,10 +80,10 @@ FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
 -- ====================================================================
--- Checking 'silver.crm_sales_details'
+-- Checando 'silver.crm_sales_details'
 -- ====================================================================
--- Check for Invalid Dates
--- Expectation: No Invalid Dates
+-- Checando para datas inválidas
+-- Esperado: 0 resultados
 SELECT 
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
@@ -92,16 +92,16 @@ WHERE sls_due_dt <= 0
     OR sls_due_dt > 20500101 
     OR sls_due_dt < 19000101;
 
--- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
--- Expectation: No Results
+-- Checagem para datas de pedidos inválidas (Order Date > Shipping/Due Dates)
+-- Esperado: 0 resultados
 SELECT 
     * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
--- Check Data Consistency: Sales = Quantity * Price
--- Expectation: No Results
+-- Checagem de consistência de dados: Sales precisa ser = Quantity * Price
+-- Esperado: 0 resultados
 SELECT DISTINCT 
     sls_sales,
     sls_quantity,
@@ -117,35 +117,35 @@ WHERE sls_sales != sls_quantity * sls_price
 ORDER BY sls_sales, sls_quantity, sls_price;
 
 -- ====================================================================
--- Checking 'silver.erp_cust_az12'
+-- Checando 'silver.erp_cust_az12'
 -- ====================================================================
--- Identify Out-of-Range Dates
--- Expectation: Birthdates between 1924-01-01 and Today
+-- Identificar datas com abrangência inesperada
+-- Esperado: Birthdates entre 1924-01-01 e Hoje
 SELECT DISTINCT 
     bdate 
 FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' 
    OR bdate > GETDATE();
 
--- Data Standardization & Consistency
+-- Consistência e padronização de dados
 SELECT DISTINCT 
     gen 
 FROM silver.erp_cust_az12;
 
 -- ====================================================================
--- Checking 'silver.erp_loc_a101'
+-- Checando 'silver.erp_loc_a101'
 -- ====================================================================
--- Data Standardization & Consistency
+-- Consistência e padronização de dados
 SELECT DISTINCT 
     cntry 
 FROM silver.erp_loc_a101
 ORDER BY cntry;
 
 -- ====================================================================
--- Checking 'silver.erp_px_cat_g1v2'
+-- Checando 'silver.erp_px_cat_g1v2'
 -- ====================================================================
--- Check for Unwanted Spaces
--- Expectation: No Results
+-- Checando para espaços desnecessários
+-- Esperado: 0 resultados
 SELECT 
     * 
 FROM silver.erp_px_cat_g1v2
@@ -153,7 +153,7 @@ WHERE cat != TRIM(cat)
    OR subcat != TRIM(subcat) 
    OR maintenance != TRIM(maintenance);
 
--- Data Standardization & Consistency
+-- Consistência e padronização de dados
 SELECT DISTINCT 
     maintenance 
 FROM silver.erp_px_cat_g1v2;
