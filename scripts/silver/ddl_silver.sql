d5c4e27c5202709f701e7400b9dde09fff4ce081
Eike Hirsch <108685558+eikehirsch@users.noreply.github.com>
2025-04-20 14:40:04 -0300

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,10 +1,12 @@
 
 USE DataWarehouse;
 
+-- Dropa tabela silver.crm_cust_info
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 GO
 
+-- Cria tabela silver.crm_cust_info
 CREATE TABLE silver.crm_cust_info (
 cst_id INT,
 cst_key NVARCHAR(50),
@@ -16,10 +18,12 @@ cst_create_date DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
 
+-- Dropa tabela silver.crm_prd_info
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
 GO
 
+-- Cria tabela silver.crm_prd_info
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
 	cat_id		 NVARCHAR(50),
@@ -33,10 +37,12 @@ CREATE TABLE silver.crm_prd_info (
 );
 GO
 
+-- Dropa tabela silver.crm_sales_details
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
 GO
 
+-- Cria tabela silver.crm_sales_details
 CREATE TABLE silver.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
     sls_prd_key  NVARCHAR(50),
@@ -51,10 +57,12 @@ CREATE TABLE silver.crm_sales_details (
 );
 GO
 
+-- Dropa tabela silver.erp_loc_a101
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE silver.erp_loc_a101;
 GO
 
+-- Cria tabela silver.erp_loc_a101
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50),
@@ -62,10 +70,12 @@ CREATE TABLE silver.erp_loc_a101 (
 );
 GO
 
+-- Dropa tabela silver.erp_cust_az12
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE silver.erp_cust_az12;
 GO
 
+-- Cria tabela silver.erp_cust_az12
 CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
@@ -74,10 +84,12 @@ CREATE TABLE silver.erp_cust_az12 (
 );
 GO
 
+-- Dropa tabela silver.erp_px_cat_g1v2
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
 GO
 
+-- Dropa tabela silver.erp_px_cat_g1v2
 CREATE TABLE silver.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
